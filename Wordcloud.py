76520f6abbb18f7cb708390dbc700237a5040722
Wei Wei (Vivi) <46175801+WeiWeiPKUFLS@users.noreply.github.com>
2023-01-30 16:40:06 +0000

    Update Wordcloud.py

--- Wordcloud.py
+++ Wordcloud.py
@@ -4,7 +4,6 @@ import jieba
 text_from_file_with_apath = open('search and discovery3.txt').read()
 wordlist_after_jieba = jieba.cut(text_from_file_with_apath, cut_all = True)
 wl_space_split = " ".join(wordlist_after_jieba)
-Prefix dict has been built successfully.
 stop_words = open(r"Baidu Stopwords.txt",encoding ="utf-8").read().split("\n")
 my_wordcloud = WordCloud(font_path = 'Songti.ttc', stopwords=stop_words, background_color="white").generate(wl_space_split)
 plt.imshow(my_wordcloud)
