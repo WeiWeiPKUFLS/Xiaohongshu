de8dee660a1a844fbf2893afe72dcaaa717026e6
Wei Wei (Vivi) <46175801+WeiWeiPKUFLS@users.noreply.github.com>
2023-02-23 10:33:12 +0000

    Update Xiaohongshu notes collection.py

--- Xiaohongshu notes collection.py
+++ Xiaohongshu notes collection.py
@@ -28,6 +28,9 @@ f.write('Publishdate: ' + publishdate + '\n' + '\n')
 f.close()
 
 
+
+
+#对于图文笔记
 #抽取图片链接
 urls = []
 for div in soup.find_all("div", class_="swiper-slide zoom-in"):
@@ -42,91 +45,129 @@ for i, url in enumerate(urls):
     with open(filename, 'wb') as f:
         f.write(response.content)
 
+	
+	
+#对于视频笔记	
+#找到要抽取视频和视频缩略图的那个部分作为script_text
+script_element = soup.select_one('body > script:nth-child(3)')
+script_text = script_element.string
 
+#抽取视频并保存
+json_str = script_text.split("window.__INITIAL_STATE__=")[1]
+pattern = r'"backupUrls":\s*\[([\s\S]*?)\]'
+backup_urls_str = re.search(pattern, json_str).group(1)
+first_backup_url = backup_urls_str.strip('"').split(',')[0].strip().rstrip('"')
+
+decoded_url = urllib.parse.unquote(first_backup_url)
+decoded_url = first_backup_url.encode().decode('unicode_escape')
+decoded_url = decoded_url.replace('\\', '')
+print(decoded_url)
+
+response1 = requests.get(decoded_url)
+with open('/Users/weiwei/Documents/comment/Blogger case 2 video.mp4', 'wb') as f:
+    f.write(response1.content)
+
+#抽取视频缩略图的链接
+for match in re.finditer('"url":"(.*?)"',script_text):
+    image_url = match.group(1).encode().decode('unicode_escape')
+    print(image_url)
+response2 = requests.get(image_url)
+with open('/Users/weiwei/Documents/comment/Blogger case 2 videothumbnail.jpg', 'wb') as f:
+	f.write(response2.content)	
+	
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 #抽取评论(需要对应用户，时间，然后还有下属的评论)
-comments = []
-replies = soup.find('div', class_='all-tip').find_all('p', class_='content')
-for reply in replies:
-    comment = reply.text
-    print(comment)
-    comments.append(comment)
-commenters = []
-usernames = soup.find_all('h4', class_="user-nickname")
-for i in usernames:
-    name = i.text
-    nam = name.translate(non_bmp_map) + ':'
-    print (nam)
-    commenters.append(nam)
-commenttimes = []
-replytimes = soup.find_all('span', class_="publish-time")
-for replytime in replytimes:
-    commenttime = replytime.text
-    print(commenttime)
-    commenttimes.append(commenttime)
-import pandas as pd
-commentdata = pd.DataFrame({'commenters':commenters,'comments':comments, 'comment-time': commenttimes})
-commentdata.to_csv('Blogger A self-selected note 7 text.txt',index=False,encoding='utf_8_sig', mode='a')
+#comments = []
+#replies = soup.find('div', class_='all-tip').find_all('p', class_='content')
+#for reply in replies:
+#    comment = reply.text
+#    print(comment)
+#    comments.append(comment)
+#commenters = []
+#usernames = soup.find_all('h4', class_="user-nickname")
+#for i in usernames:
+#    name = i.text
+#    nam = name.translate(non_bmp_map) + ':'
+#    print (nam)
+#    commenters.append(nam)
+#commenttimes = []
+#replytimes = soup.find_all('span', class_="publish-time")
+#for replytime in replytimes:
+#    commenttime = replytime.text
+#    print(commenttime)
+#    commenttimes.append(commenttime)
+#import pandas as pd
+#commentdata = pd.DataFrame({'commenters':commenters,'comments':comments, 'comment-time': commenttimes})
+#commentdata.to_csv('Blogger A self-selected note 7 text.txt',index=False,encoding='utf_8_sig', mode='a')
 
 #处理评论的评论
-subcomments = []
-subcommenters = []
-subreplies = soup.find('div', class_='all-tip').find_all('p', class_='reply-content')
-for subreply in subreplies:
-    subrep = subreply.text
-    print(subrep)
-    subcomments.append(subrep)
+#subcomments = []
+#subcommenters = []
+#subreplies = soup.find('div', class_='all-tip').find_all('p', class_='reply-content')
+#for subreply in subreplies:
+#    subrep = subreply.text
+#    print(subrep)
+#    subcomments.append(subrep)
 
-subrepliers = soup.find('div', class_='all-tip').find_all('span', class_='replier')
-for subreplier in subrepliers:
-    suber = subreplier.text
-    print(suber)
-    subcommenters.append(suber)
-subcommentdata = pd.DataFrame({'subcommenters': subcommenters,'subcomments': subcomments})
-subcommentdata.to_csv('Blogger A self-selected note 7 text.txt',index=False,encoding='utf_8_sig', mode='a')
+#subrepliers = soup.find('div', class_='all-tip').find_all('span', class_='replier')
+#for subreplier in subrepliers:
+#    suber = subreplier.text
+#    print(suber)
+#    subcommenters.append(suber)
+#subcommentdata = pd.DataFrame({'subcommenters': subcommenters,'subcomments': subcomments})
+#subcommentdata.to_csv('Blogger A self-selected note 7 text.txt',index=False,encoding='utf_8_sig', mode='a')
 
 #视频下载
-videoSrc = soup.find('div', class_ = 'videoframe').find('video').get('src')
-urllib.request.urlretrieve(videoSrc,'Blogger A self-selected note 7.mp4')
+#videoSrc = soup.find('div', class_ = 'videoframe').find('video').get('src')
+#urllib.request.urlretrieve(videoSrc,'Blogger A self-selected note 7.mp4')
 #抽取video transcript
-f2 = open('Blogger A self-selected note 7 transcript.txt', 'a', encoding='utf-8')
-transcript = soup.find('p', class_ = 'generated-text').get_text()
-f2.write('(transcript)' + transcript + '\n')
-f2.close()
-
-
+#f2 = open('Blogger A self-selected note 7 transcript.txt', 'a', encoding='utf-8')
+#transcript = soup.find('p', class_ = 'generated-text').get_text()
+#f2.write('(transcript)' + transcript + '\n')
+#f2.close()
 
 #抽取视频
-videoSrc = soup.find('div', class_ = 'videoframe').find('video').get('src')
-urllib.request.urlretrieve(videoSrc,'participant 1 self-selected 7.mp4')
+#videoSrc = soup.find('div', class_ = 'videoframe').find('video').get('src')
+#urllib.request.urlretrieve(videoSrc,'participant 1 self-selected 7.mp4')
 
 #抽取视频transcript
-f2 = open('participant 1 self-selected note 7 transcript.txt', 'a', encoding='utf-8')
-transcript = soup.find('p', class_ = 'generated-text').get_text()
-f2.write('(transcript)' + transcript + '\n')
-f2.close()
+#f2 = open('participant 1 self-selected note 7 transcript.txt', 'a', encoding='utf-8')
+#transcript = soup.find('p', class_ = 'generated-text').get_text()
+#f2.write('(transcript)' + transcript + '\n')
+#f2.close()
 
 #抽取图片
-image = soup.find('div', class_ = 'inner').find_all('style')
+#image = soup.find('div', class_ = 'inner').find_all('style')
 #抽取图片的链接
-picUrls = []
-pics = soup.find('div', class_ = 'small-pic').find_all('div')
-for pic in pics:
-    picUrl = 'http:' + pic.find('i', class_ = 'img').get('style')[21:-32]
-    picUrls.append(picUrl)
-for i in picUrls:
-	image_url = "'http://' + i"
-	headers ={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
-	r = requests.get(image_url,headers=headers)
-	f = open("i.jpg", 'wb')
-	f.write(r.content)
-	f.close()
+#picUrls = []
+#pics = soup.find('div', class_ = 'small-pic').find_all('div')
+#for pic in pics:
+#   picUrl = 'http:' + pic.find('i', class_ = 'img').get('style')[21:-32]
+#    picUrls.append(picUrl)
+#for i in picUrls:
+#	image_url = "'http://' + i"
+#	headers ={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
+#	r = requests.get(image_url,headers=headers)
+#	f = open("i.jpg", 'wb')
+#	f.write(r.content)
+#	f.close()
 
 #直接从相关链接下载图片（要补充http://，是不是可以‘http://’+'抽取出来的链接')
-image_url='http://ci.xiaohongshu.com/5b123788-3c47-8610-e910-a37c6dc3be4f?imageView2/2/w/1080/format/jpg'
-headers ={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
-r = requests.get(image_url,headers=headers)
+#image_url='http://ci.xiaohongshu.com/5b123788-3c47-8610-e910-a37c6dc3be4f?imageView2/2/w/1080/format/jpg'
+#headers ={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
+#r = requests.get(image_url,headers=headers)
 
 #默认放进Documents(还需要改具体存放地址)
 f = open("1.jpg",'wb')
@@ -135,16 +176,16 @@ f.close()
 
 
 #试一下用xpath
-from lxml import etree
-maintext = soup.find('div', class_ = 'content')
-tree = etree.HTML(maintext)
-li_list = tree.xpath('//div[@class="content"]/p')
-fp = open('test1.txt','w',encoding='utf-8')
-for li in li_list:
-	paragraph = li.xpath('./div[2]//h3/text()')[0] + li.xpath('./div[2]/div[2]/p/span/text()')[0]
+#from lxml import etree
+#maintext = soup.find('div', class_ = 'content')
+#tree = etree.HTML(maintext)
+#li_list = tree.xpath('//div[@class="content"]/p')
+#fp = open('test1.txt','w',encoding='utf-8')
+#for li in li_list:
+	#paragraph = li.xpath('./div[2]//h3/text()')[0] + li.xpath('./div[2]/div[2]/p/span/text()')[0]
 
 
-a = soup.find_all('span', class_="username")
+#a = soup.find_all('span', class_="username")
 
 
 
