ada4493de6fd664936a41199c7c3eb7ebb60b4ba
Wei Wei (Vivi) <46175801+WeiWeiPKUFLS@users.noreply.github.com>
2023-03-17 12:40:29 +0000

    Update Monthly notes bar chart.py

--- Monthly notes bar chart.py
+++ Monthly notes bar chart.py
@@ -1,22 +1,41 @@
 import pandas as pd
+import datetime
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('/Users/weiwei/Documents/Jiban/Jiban notes list output 16Mar23.csv')
+df = pd.read_csv('/Users/weiwei/Documents/Huanhuan/Huanhuan notes list output 15Mar23.csv')
 
+# Convert the 'time' column to datetime format
 df['time'] = pd.to_datetime(df['time'], format='%d/%m/%Y %H:%M')
-df['month_year'] = df['time'].dt.strftime('%b-%Y')
 
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.bar(df['month_year'].unique(), df.groupby(['month_year']).size())
+# Create a new column for year and month
+df['year_month'] = df['time'].dt.strftime('%Y-%m')
 
-ax.set_xlim(left=-0.7, right=len(df['month_year'].unique())-0.3)
+# Count the number of URLs for each year_month
+url_count = df.groupby('year_month')['webpage_url'].count()
 
-ax.set_xlabel('Month and Year')
-ax.set_ylabel('Number of Notes')
-ax.set_xticklabels(df['month_year'].unique(), rotation=45)
+# Create a list of all the year_month values
+year_month_list = pd.date_range(start=url_count.index.min(), end=url_count.index.max(), freq='MS').strftime('%Y-%m').tolist()
 
-# add text labels above each bar
-for i in ax.containers:
-    ax.bar_label(i, label_type='edge', fontsize=10)
+# Create a dictionary to hold the counts for each year_month
+url_count_dict = dict.fromkeys(year_month_list, 0)
 
+# Update the counts in the dictionary with the values from the url_count series
+url_count_dict.update(url_count.to_dict())
+
+# Create a bar chart
+plt.bar(url_count_dict.keys(), url_count_dict.values())
+
+# Add text labels to the bars
+for i, v in enumerate(url_count_dict.values()):
+    plt.text(i, v, str(v), ha='center', va='bottom')
+
+# Add x-label and y-label
+plt.xlabel('Month and Year')
+plt.ylabel('Number of Notes')
+
+# Rotate the x-axis labels
+plt.xticks(rotation=90)
+
+# Show the plot
 plt.show()
+
