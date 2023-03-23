5995600fc8a3e99e45d3ff456a780f427f5e3ccb
Wei Wei (Vivi) <46175801+WeiWeiPKUFLS@users.noreply.github.com>
2023-03-23 14:25:21 +0000

    Update Xiaohongshu comments collection.py

--- Xiaohongshu comments collection.py
+++ Xiaohongshu comments collection.py
@@ -56,3 +56,62 @@ dates = [datetime.datetime.fromtimestamp(ts/1000.0).date() for ts in timestamps]
 # print dates
 for d in dates:
     print(d)
+    
+    
+#批量处理文件
+import os
+import json
+import pandas as pd
+from datetime import datetime
+
+# set the directory path记得改地址
+directory = "/Users/weiwei/Documents/Participant notes/Zhichuantao/Zhichuantao video"
+
+# loop through all the json files in the directory
+for file in os.listdir(directory):
+    if file.endswith("comments.json"):
+        # extract the file name of the txt file
+        txt_file_name = file.split("comments")[0].strip() + ".txt"
+        # create the full path for both the json and txt files
+        json_file_path = os.path.join(directory, file)
+        txt_file_path = os.path.join(directory, txt_file_name)
+
+        # extract the comments data from the json file
+        with open(json_file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        comments = []
+        for comment in data['data']['comments']:
+            sub_comments_list = []
+            for sub_comment in comment.get('sub_comments', []):
+                sub_create_time = datetime.fromtimestamp(sub_comment['create_time'] // 1000)
+                sub_like_count = sub_comment.get('like_count', None)
+                sub_comments_list.append([sub_comment['content'], 
+                                          sub_comment['user_info']['nickname'], 
+                                          sub_create_time.strftime("%d/%m/%Y %H:%M"),sub_like_count])
+                
+            create_time = datetime.fromtimestamp(comment['create_time'] // 1000)
+            like_count = comment.get('like_count', None)
+            comments.append([comment['content'], 
+                             comment['user_info']['nickname'], 
+                             create_time.strftime("%d/%m/%Y %H:%M"),
+                             like_count,
+                             sub_comments_list])
+
+        # save the comments data to a txt file
+        df = pd.DataFrame(comments, columns=['content', 'nickname', 'create_time', 'like_count', 'sub_comments'])
+        with open(txt_file_path, 'a', encoding='utf-8') as f:
+            for i, row in df.iterrows():
+                f.write(f"{i+1}. {row['content']}\n")
+                f.write(f"   Posted by: {row['nickname']} on {row['create_time']},")
+                if not pd.isna(row['like_count']):
+                    f.write(f" Likes: {row['like_count']}\n")
+                if not row['sub_comments']:
+                    f.write("   No sub-comments\n")
+                else:
+                    f.write("   Sub-comments:\n")
+                    for j, sub_comment in enumerate(row['sub_comments']):
+                        f.write(f"     {j+1}. {sub_comment[0]} \n")
+                        f.write(f"         Posted by: {sub_comment[1]} on {sub_comment[2]}, Likes: {sub_comment[3]}\n")
+                f.write("\n")
+
+
